b86300ddd978204a24860321b93af210a154f9b3
Luiz Henrique Araujo Mello <lhamello@gmail.com>
2019-08-19 19:57:23 -0300

    Profile para teste.

--- src/test/java/br/com/lhamello/springbootpetshop/repository/AnimalRepositoryIT.java
+++ src/test/java/br/com/lhamello/springbootpetshop/repository/AnimalRepositoryIT.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,6 +16,7 @@ import br.com.lhamello.springbootpetshop.model.Animal;
 
 @DataJpaTest
 @RunWith(SpringRunner.class)
+@ActiveProfiles("test-jpa")
 public class AnimalRepositoryIT {
 
 	@Autowired
